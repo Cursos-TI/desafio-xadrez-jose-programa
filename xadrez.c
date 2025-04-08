95d49f8af653b919cd9c9ca1ee2d51277c5c559e
jose-programa <resenhista45@gmail.com>
2025-04-08 23:47:48 +0000

    desafio nivel aventureiro - tema 3

--- xadrez.c
+++ xadrez.c
@@ -2,28 +2,36 @@
 
 int main() {
     
-    printf("Movimento da Torre (5 casas para a direita):\n");
-    for (int i = 0; i < 5; i++) {
+    printf("Movimento da Torre:\n");
+    for(int i = 0; i < 5; i++) {
+        printf("Cima\n");
+    }
+    
+    
+    printf("\nMovimento do Bispo:\n");
+    int i = 0;
+    while(i < 5) {
         printf("Direita\n");
+        i++;
     }
-    printf("\n");
-
     
-    printf("Movimento do Bispo (5 casas na diagonal superior direita):\n");
-    int casas_bispo = 0;
-    while (casas_bispo < 5) {
-        printf("Cima, Direita\n");
-        casas_bispo++;
+    
+    printf("\nMovimento do Cavalo:\n");
+    
+    
+    const int passos_baixo = 2;  
+    const int passos_esquerda = 1; 
+    
+    
+    for(int j = 0; j < passos_baixo; j++) {
+        printf("Baixo\n");
     }
-    printf("\n");
-
     
-    printf("Movimento da Rainha (8 casas para a esquerda):\n");
-    int casas_rainha = 0;
+    int k = 0;
     do {
         printf("Esquerda\n");
-        casas_rainha++;
-    } while (casas_rainha < 8);
-
+        k++;
+    } while(k < passos_esquerda);
+    
     return 0;
 }
